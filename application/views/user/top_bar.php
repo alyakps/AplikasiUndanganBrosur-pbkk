fb65eaff547ec084b2b3a321f56a6e19fabb6380
unknown <aps.alyaputrisalma@gmail.com>
2023-12-18 01:54:16 +0700

    final project

---
+++ application/views/user/top_bar.php
@@ -0,0 +1,17 @@
+    <div class="border-bottom top-bar py-2">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+
+          </div>
+          <div class="col-md-6">
+            <ul class="social-media">
+              <li><a href="#" class="p-2"><span class="icon-facebook"></span></a></li>
+              <li><a href="#" class="p-2"><span class="icon-twitter"></span></a></li>
+              <li><a href="#" class="p-2"><span class="icon-instagram"></span></a></li>
+              <li><a href="#" class="p-2"><span class="icon-linkedin"></span></a></li>
+            </ul>
+          </div>
+        </div>
+      </div> 
+    </div>
